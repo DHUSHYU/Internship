6924d002cc08c203069411be9d8f105d8698966c
DHUSHYU <smartworkdhushyu@gmail.com>
2025-01-29 21:47:40 +0530

    done

--- server/server.js
+++ server/server.js
@@ -11,12 +11,18 @@ const PORT = 5000;
 
 // Middleware
 app.use(cors());
-app.use(cors({
+const allowedOrigins = ["https://dhushyu-portfolio.vercel.app", "http://localhost:5000"];
 
-  origin:"https://dhushyu-portfolio.vercel.app",
-  methods:"GET,POST,PUT,DELETE",
-  credentials:true
-}));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 
 app.use(express.json());
 app.use(bodyParser.json());
